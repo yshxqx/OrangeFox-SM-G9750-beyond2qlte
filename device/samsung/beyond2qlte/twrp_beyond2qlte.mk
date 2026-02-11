c9973fee915087c51be5481680eefe1e91a5a25c
yshxqx <69250113+yshxqx@users.noreply.github.com>
2026-02-11 22:35:23 +0800

    Add TWRP device configuration for Galaxy S10+

---
+++ device/samsung/beyond2qlte/twrp_beyond2qlte.mk
@@ -0,0 +1,10 @@
+$(call inherit-product, device/samsung/beyond2qlte/device.mk)
+
+PRODUCT_DEVICE := beyond2qlte
+PRODUCT_NAME := twrp_beyond2qlte
+PRODUCT_MODEL := SM-G9750
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_BRAND := samsung
+
+OF_VARIANT_FULL := beyond2qlte
+OF_DEVICE_NAME := Galaxy S10+
